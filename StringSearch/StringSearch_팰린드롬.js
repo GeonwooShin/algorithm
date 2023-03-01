0e28a9906804581e0a559d8989d600e6d284b033
GeonwooShin <rjsdnslawkd@naver.com>
2023-03-01 20:58:46 +0900

    feat : 문제 풀이

---
+++ StringSearch/StringSearch_팰린드롬.js
@@ -0,0 +1,13 @@
+function solution(str) {
+  let answer = "YES";
+  str = str.toLowerCase().replace(/[^a-z]/g, "");
+  for (let i = 0; i < str.length / 2; i++) {
+    if (str[i] !== str[str.length - 1 - i]) {
+      return "NO";
+    }
+  }
+  return answer;
+}
+
+let str = "found, time: study; Yduts; emit, 7Dnuof";
+console.log(solution(str));
