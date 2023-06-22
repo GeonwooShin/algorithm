7876a9a2e37f88378454a606bab9839372cfa106
GeonwooShin <rjsdnslawkd@naver.com>
2023-06-22 23:14:30 +0900

    del: 파일 이동

--- Basic/Basic_연필개수.js
+++
@@ -1,6 +0,0 @@
-function solution(studentNum) {
-  let answer = Math.ceil(studentNum / 12);
-  return answer;
-}
-
-console.log(solution(882));
--- Basic/Basic_최솟값구하기.js
+++
@@ -1,9 +0,0 @@
-function solution(arr) {
-  let answer;
-  arr.sort((a, b) => a - b);
-  answer = arr;
-  return answer;
-}
-
-let numArr = [5, 3, 7, 11, 2, 15, 17];
-console.log(solution(numArr));
