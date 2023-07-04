ed260a926a480b1d5c1dc24664f1d160052786d7
GeonwooShin <rjsdnslawkd@naver.com>
2023-07-05 01:04:45 +0900

    [Silver4]_1018

---
+++ BOJ/Bruteforce/1018_체스판.js
@@ -0,0 +1,39 @@
+const str =
+  "10 13\nBBBBBBBBWBWBW\nBBBBBBBBBWBWB\nBBBBBBBBWBWBW\nBBBBBBBBBWBWB\nBBBBBBBBWBWBW\nBBBBBBBBBWBWB\nBBBBBBBBWBWBW\nBBBBBBBBBWBWB\nWWWWWWWWWWBWB\nWWWWWWWWWWBWB";
+// const str =
+//   "8 8\nWBWBWBWB\nBWBWBWBW\nWBWBWBWB\nBWBBBWBW\nWBWBWBWB\nBWBWBWBW\nWBWBWBWB\nBWBWBWBW";
+const input = str.toString().trim().split("\n");
+const [M, N] = input
+  .shift()
+  .split(" ")
+  .map((x) => Number(x));
+
+const goodEx1 = [];
+const goodEx2 = [];
+for (let i = 0; i < 8; i++) {
+  if (i % 2 === 0) goodEx1.push("WBWBWBWB");
+  else goodEx1.push("BWBWBWBW");
+}
+for (let i = 0; i < 8; i++) {
+  if (i % 2 === 1) goodEx2.push("WBWBWBWB");
+  else goodEx2.push("BWBWBWBW");
+}
+let repaintEx1Arr = [];
+let repaintEx2Arr = [];
+
+for (let k = 0; k < M - 7; k++) {
+  for (let t = 0; t < N - 7; t++) {
+    let repaintEx1 = 0;
+    let repaintEx2 = 0;
+    for (let i = 0; i < 8; i++) {
+      for (let j = 0; j < 8; j++) {
+        if (input[i + k][j + t] !== goodEx1[i][j]) repaintEx1++;
+        if (input[i + k][j + t] !== goodEx2[i][j]) repaintEx2++;
+      }
+    }
+    repaintEx1Arr.push(repaintEx1);
+    repaintEx2Arr.push(repaintEx2);
+  }
+}
+
+console.log(Math.min(...repaintEx1Arr, ...repaintEx2Arr));
