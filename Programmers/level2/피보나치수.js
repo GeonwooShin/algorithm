d24ad3c1f2818614a776b956a1d28382a31c8df5
GeonwooShin <rjsdnslawkd@naver.com>
2023-09-04 23:15:27 +0900

    [Programmers]_피보나치수

---
+++ Programmers/level2/피보나치수.js
@@ -0,0 +1,11 @@
+function solution(n) {
+  let result = [0, 1];
+  for (let i = 2; i <= n; i++) {
+    let sum = result[i - 2] + result[i - 1];
+    result.push(sum % 1234567);
+  }
+  return result[n];
+}
+
+const n = 5;
+console.log(solution(n));
