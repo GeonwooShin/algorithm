1cba196684d7405c6007bf6087caad9e05b8d750
GeonwooShin <rjsdnslawkd@naver.com>
2023-10-12 19:39:31 +0900

    [Programmers]_2xn타일링

---
+++ Programmers/level2/2xn타일링.js
@@ -0,0 +1,10 @@
+function solution(n) {
+  const dp = [0, 1, 2];
+  for (let i = 3; i <= n; i++) {
+    dp[i] = (dp[i - 2] + dp[i - 1]) % 1000000007;
+  }
+  return dp[n];
+}
+
+const n = 4;
+console.log(solution(n));
