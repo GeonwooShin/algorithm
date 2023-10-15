84443a466384900cdeab428d4628156c825fe677
GeonwooShin <rjsdnslawkd@naver.com>
2023-10-15 18:23:32 +0900

    [Programmers]_택배상자

--- Programmers/level2/택배상자.js
+++ Programmers/level2/택배상자.js
@@ -21,43 +21,24 @@ function solution(order) {
 const order = [4, 3, 1, 2, 5];
 console.log(solution(order));
 
-// 쌓아야하는 순서 4 3 1 2 5
-// 컨테이너 벨트에 오는 순서 1 2 3 4 5
-
-// 보조 컨테이너 벨트에 쌓는 순서 1 2
-// 컨테이너 벨트에 남아있는 박스 5
-
-// 4 - 3
-
-// 보조 컨테이너 벨트에서 뺄 수 있는 것은 2와 컨테이너 벨트에서 뺄 수 있는 것은 5
-// 하지만 빼야하는 것은 1이므로 쌓을 수 있는 상자는 두 개
-
-// 쌓아야하는 순서 5 4 3 2 1
-// 컨테이너 벨트에 오는 순서 1 2 3 4 5
-
-// 보조 컨테이너 벨트에 쌓는 순서 1 2 3 4 5
-// 컨테이너 벨트에 남아있는 박스
-
-// 5 - 4 - 3 - 2 - 1
-
-// 뺄 수 있는 상자는 5개
+// 런타임 에러
 
 // function solution(order) {
 //   let answer = 0;
 //   const max = Math.max(...order);
 //   const container = Array.from({ length: max }, (_, idx) => idx + 1);
-//   const assistContainer = [];
+//   const subContainer = [];
 //   for (let i = 0; i < order.length; i++) {
 //     if (container.includes(order[i])) {
 //       while (order[i] !== container[0]) {
-//         assistContainer.push(container.shift());
+//         subContainer.push(container.shift());
 //       }
 //       if (container[0] === order[i]) {
 //         container.shift();
 //         answer++;
 //       }
-//     } else if (assistContainer.at(-1) === order[i]) {
-//       assistContainer.pop();
+//     } else if (subContainer.at(-1) === order[i]) {
+//       subContainer.pop();
 //       answer++;
 //     } else return answer;
 //   }
