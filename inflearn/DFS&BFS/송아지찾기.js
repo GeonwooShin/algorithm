2964c27b1ae615e6daf3af99a18e59db21c24226
GeonwooShin <rjsdnslawkd@naver.com>
2023-10-26 23:40:31 +0900

    refactor: E가 S보다 큰 경우 고려

--- inflearn/DFS&BFS/송아지찾기.js
+++ inflearn/DFS&BFS/송아지찾기.js
@@ -1,14 +1,15 @@
 function solution(S, E) {
-  const distance = Array.from(Array(E + 1), () => 0);
-  const ch = Array.from(Array(E + 1), () => 0);
+  const MAX_COUNT = 10000;
+  const distance = Array.from(Array(MAX_COUNT + 1), () => 0);
+  const ch = Array.from(Array(MAX_COUNT + 1), () => 0);
   const queue = [S];
   while (queue.length) {
     const location = queue.shift();
+    if (ch[location]) continue;
     ch[location] = 1;
     for (let move of [location - 1, location + 1, location + 5]) {
       if (move === E) return distance[location] + 1;
-      if (move > 0 && move <= 10000 && ch[move] === 0) {
-        ch[move] = 1;
+      if (move > 0 && move <= MAX_COUNT) {
         queue.push(move);
         distance[move] = distance[location] + 1;
       }
@@ -16,7 +17,7 @@ function solution(S, E) {
   }
 }
 
-const S = 5;
-const E = 14;
+const S = 8;
+const E = 3;
 
 console.log(solution(S, E));
