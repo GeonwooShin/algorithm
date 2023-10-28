dcce82b930091f2593ad97527a999cae22aae377
GeonwooShin <rjsdnslawkd@naver.com>
2023-10-28 20:39:41 +0900

    [Programmers]_소수찾기

---
+++ Programmers/level2/소수찾기.js
@@ -0,0 +1,31 @@
+function isPrime(num) {
+  if (num < 2) return false;
+  for (let i = 2; i <= Math.sqrt(num); i++) {
+    if (num % i === 0) return false;
+  }
+  return true;
+}
+
+function solution(numbers) {
+  const n = numbers.length;
+  const ch = Array.from({ length: n }, () => 0);
+  const set = new Set();
+  function dfs(str) {
+    for (let i = 0; i < n; i++) {
+      if (str !== "") {
+        if (isPrime(parseInt(str))) {
+          set.add(parseInt(str));
+        }
+      }
+      if (ch[i] === 0) {
+        ch[i] = 1;
+        dfs(str + numbers[i]);
+        ch[i] = 0;
+      }
+    }
+  }
+  dfs("");
+  return set.size;
+}
+
+console.log(solution("011"));
