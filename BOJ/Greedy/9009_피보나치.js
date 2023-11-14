5d258d125366bbe588bbaa02d96984f2bd3122f8
GeonwooShin <rjsdnslawkd@naver.com>
2023-11-14 18:40:08 +0900

    refactor: 코드 수정

--- BOJ/Greedy/9009_피보나치.js
+++ BOJ/Greedy/9009_피보나치.js
@@ -2,21 +2,24 @@ const fs = "4\n100\n200\n12345\n1003";
 const [a, ...b] = fs.toString().trim().split("\n");
 
 const T = Number(a);
-const data = b.map(Number);
-const arr = [0, 1];
+const datas = b.map(Number);
+const fibo = [0, 1];
+
 for (let i = 2; ; i++) {
-  arr[i] = arr[i - 1] + arr[i - 2];
-  if (arr[i] > 1000000000) break;
+  fibo[i] = fibo[i - 1] + fibo[i - 2];
+  if (fibo[i] > 1000000000) break;
 }
 
-for (let i = 0; i < data.length; i++) {
+for (let data of datas) {
   const result = [];
-  let num = data[i];
-  while (num !== 0) {
-    const newArr = arr.filter((v) => v <= num);
-    const bigNum = newArr[newArr.length - 1];
-    num -= bigNum;
-    result.push(bigNum);
+  let num = data;
+  let idx = fibo.length - 1;
+  while (num > 0) {
+    if (fibo[idx] <= num) {
+      num -= fibo[idx];
+      result.push(fibo[idx]);
+    }
+    idx--;
   }
   console.log(result.reverse().join(" "));
 }
