b9128f1193f0702592726a9fc52529d078a4254a
GeonwooShin <rjsdnslawkd@naver.com>
2023-11-23 23:10:03 +0900

    refactor: 순열 로직

--- BOJ/BackTracking/15649_N과M.js
+++ BOJ/BackTracking/15649_N과M.js
@@ -2,26 +2,46 @@ const input = "4 2";
 const [N, M] = input.toString().trim().split(" ").map(Number);
 
 const numbers = Array.from({ length: N }, (_, idx) => idx + 1);
-const isVisited = Array.from({ length: N }, () => 0);
-const selected = [];
-let answer = "";
-function DFS(depth) {
-  if (depth === M) {
-    answer += selected.join(" ") + "\n";
+const answer = [];
+
+function permutations(selected, list) {
+  if (selected.length === M) {
+    answer.push(selected);
     return;
   }
-  for (let i = 1; i <= numbers.length; i++) {
-    if (isVisited[i]) continue;
-    selected.push(i);
-    isVisited[i] = 1;
-    DFS(depth + 1);
-    selected.pop();
-    isVisited[i] = 0;
+  for (let i = 0; i < list.length; i++) {
+    permutations(
+      selected.concat(list[i]),
+      numbers.filter((v) => v !== numbers[i])
+    );
   }
 }
 
-DFS(0);
+permutations([], numbers);
 console.log(answer);
+
+// const numbers = Array.from({ length: N }, (_, idx) => idx + 1);
+// const isVisited = Array.from({ length: N }, () => 0);
+// const selected = [];
+// let answer = "";
+// function DFS(depth) {
+//   if (depth === M) {
+//     answer += selected.join(" ") + "\n";
+//     return;
+//   }
+//   for (let i = 1; i <= numbers.length; i++) {
+//     if (isVisited[i]) continue;
+//     selected.push(i);
+//     isVisited[i] = 1;
+//     DFS(depth + 1);
+//     selected.pop();
+//     isVisited[i] = 0;
+//   }
+// }
+
+// DFS(0);
+// console.log(answer);
+
 // const isVisited = Array.from({ length: N }, () => 0);
 // function range(n) {
 //   const results = [];
